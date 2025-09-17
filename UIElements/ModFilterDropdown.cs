f506088b897b6754dff97f2804027a9ceb84ae31
mcar <85767699+r00tens@users.noreply.github.com>
2025-09-17 20:53:45 +0200

    feat: truncate long mod names and show full in tooltip

--- UIElements/ModFilterDropdown.cs
+++ UIElements/ModFilterDropdown.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Graphics;
 using Terraria.GameContent;
 using Terraria.GameContent.UI.Elements;
+using Terraria.ModLoader.UI;
 using Terraria.UI;
 
 namespace RecipeBrowser.UIElements;
@@ -110,20 +113,28 @@ internal sealed class ModFilterDropdown : UIPanel
 
 	private sealed class ModFilterDropdownRow : UIPanel
 	{
+		private const float TextScale = 0.85f;
+
+		private readonly string _fullText;
+		private readonly UIText _label;
+		
 		private bool _selected;
+		private bool _hasComputedTruncation;
+		private bool _isTruncated;
 
 		private int Index { get; }
 
 		public ModFilterDropdownRow(int index, string displayText, bool selected, Action<int> onSelect)
 		{
-			Index = index;
+			_fullText = displayText;
 			_selected = selected;
+			Index = index;
 
 			Width.Set(0f, 1f);
 			Height.Set(30f, 0f);
 
-			var label = new UIText(displayText, 0.85f) { VAlign = 0.5f };
-			Append(label);
+			_label = new UIText(displayText, TextScale) { VAlign = 0.5f };
+			Append(_label);
 
 			OnLeftClick += (_, _) => onSelect?.Invoke(Index);
 			OnMouseOver += (_, _) =>
@@ -145,10 +156,75 @@ internal sealed class ModFilterDropdown : UIPanel
 			Refresh();
 		}
 
+		protected override void DrawSelf(SpriteBatch spriteBatch)
+		{
+			base.DrawSelf(spriteBatch);
+			
+			if (!_hasComputedTruncation)
+			{
+				ComputeTruncationOnce();
+				_hasComputedTruncation = true;
+			}
+			
+			if (_isTruncated && IsMouseHovering)
+			{
+				UICommon.TooltipMouseText(_fullText);
+			}
+		}
+		
 		private void Refresh()
 		{
 			BackgroundColor = _selected ? Color.DarkRed : Color.Transparent;
 			BorderColor = _selected ? Color.DarkRed : Color.Transparent;
+		}
+		
+		private void ComputeTruncationOnce()
+		{
+			float availablePx = GetInnerDimensions().Width;
+			if (availablePx <= 0f)
+			{
+				_label.SetText(string.Empty);
+				_isTruncated = true;
+				return;
+			}
+			
+			DynamicSpriteFont font = FontAssets.MouseText.Value;
+			float maxUnits = availablePx / TextScale;
+			
+			float fullUnits = font.MeasureString(_fullText).X;
+			if (fullUnits <= maxUnits)
+			{
+				_label.SetText(_fullText);
+				_isTruncated = false;
+				return;
+			}
+			
+			const string ellipsis = "...";
+			float ellipsisUnits = font.MeasureString(ellipsis).X;
+			if (ellipsisUnits > maxUnits)
+			{
+				_label.SetText(string.Empty);
+				_isTruncated = true;
+				return;
+			}
+			
+			int minLength = 0, maxLength = _fullText.Length;
+			while (minLength < maxLength)
+			{
+				int candidateLength = (minLength + maxLength + 1) >> 1;
+				float units = font.MeasureString(_fullText[..candidateLength]).X + ellipsisUnits;
+				if (units <= maxUnits)
+				{
+					minLength = candidateLength;
+				}
+				else
+				{
+					maxLength = candidateLength - 1;
+				}
+			}
+			
+			_label.SetText(_fullText[..minLength] + ellipsis);
+			_isTruncated = true;
 		}
 	}
 }
